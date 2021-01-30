3a910e37e325f453adadaff9430230ff48756152
emonibnsalim <emonibnsali@gmail.com>
2021-01-30 11:35:30 +0600

    ticket

--- script.js
+++ script.js
@@ -1,31 +1,59 @@
-function handleFastTicketChange(isIncrease){
-    const ticketInput = document.getElementById('fast-ticket-count');
-    const ticketCount = parseInt(ticketInput.value)
-    let ticketNewCount = ticketCount
+function handleBusTicket(busTicket, isIncrease){
+    const BusTicketInput = document.getElementById(busTicket + '-count');
+    const BusTicketCount = parseInt(BusTicketInput.value)
+    let BusTicketNewCount = BusTicketCount
     if(isIncrease == true){
-        ticketNewCount = ticketCount + 1;
+        BusTicketNewCount = BusTicketCount + 1;
     }
-    if(isIncrease == false && ticketCount > 0){
-        ticketNewCount = ticketCount - 1;
+    if(isIncrease == false && BusTicketCount > 0){
+        BusTicketNewCount = BusTicketCount - 1;
     }
-
-    ticketInput.value = ticketNewCount;
+    BusTicketInput.value = BusTicketNewCount;
+    calculateTotal()
 }
+function calculateTotal(){
+    const fastTicketCount = getInputValue('fast-ticket');
+    const ticketCount = getInputValue('ticket')
+    
+    const totalPrice = fastTicketCount * 150 + ticketCount * 100;
+    document.getElementById('total-price').innerText = '$' + totalPrice;
+}
+function getInputValue(busTicket){
+    const busTicketInput = document.getElementById(busTicket + '-count');
+    const busTicketCount = parseInt(busTicketInput.value);
+    return busTicketCount
+
+}
+
+
+// function handleFastTicketChange(isIncrease){
+//     const ticketInput = document.getElementById('fast-ticket-count');
+//     const ticketCount = parseInt(ticketInput.value)
+//     let ticketNewCount = ticketCount
+//     if(isIncrease == true){
+//         ticketNewCount = ticketCount + 1;
+//     }
+//     if(isIncrease == false && ticketCount > 0){
+//         ticketNewCount = ticketCount - 1;
+//     }
+
+//     ticketInput.value = ticketNewCount;
+// }
   
 
-function handleTicketChange(isIncrease){
-    const ticketInput = document.getElementById('ticket-count');
-    const ticketCount = parseInt(ticketInput.value)
-    let ticketNewCount = ticketCount
-    if(isIncrease == true){
-        ticketNewCount = ticketCount + 1;
-    }
-    if(isIncrease == false && ticketCount > 0){
-        ticketNewCount = ticketCount - 1;
-    }
+// function handleTicketChange(isIncrease){
+//     const ticketInput = document.getElementById('ticket-count');
+//     const ticketCount = parseInt(ticketInput.value)
+//     let ticketNewCount = ticketCount
+//     if(isIncrease == true){
+//         ticketNewCount = ticketCount + 1;
+//     }
+//     if(isIncrease == false && ticketCount > 0){
+//         ticketNewCount = ticketCount - 1;
+//     }
 
-    ticketInput.value = ticketNewCount;
-}
+//     ticketInput.value = ticketNewCount;
+// }
 // document.getElementById('ticket-increase-economy').addEventListener('click', function(){
 //     const ticketInput = document.getElementById('ticket-count');
 //     const ticketCount = parseInt(ticketInput.value)
