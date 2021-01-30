9cf975295846b9265f37ed88b17a715ecc01a342
emonibnsalim <emonibnsali@gmail.com>
2021-01-30 19:32:04 +0600

    added

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function handleBusTicket(busTicket, isIncrease){
     const BusTicketInput = document.getElementById(busTicket + '-count');
-    const BusTicketCount = parseInt(BusTicketInput.value)
+    const BusTicketCount = getInputValue(busTicket)
     let BusTicketNewCount = BusTicketCount
     if(isIncrease == true){
         BusTicketNewCount = BusTicketCount + 1;
@@ -15,15 +15,27 @@ function calculateTotal(){
     const fastTicketCount = getInputValue('fast-ticket');
     const ticketCount = getInputValue('ticket')
     
-    const totalPrice = fastTicketCount * 150 + ticketCount * 100;
-    document.getElementById('total-price').innerText = '$' + totalPrice;
+    const totalAmount = fastTicketCount * 150 + ticketCount * 100;
+    document.getElementById('total-amount').innerText = '$' + totalAmount;
+    
+    const vatCharge = totalAmount * 0.1;
+    document.getElementById('vat').innerText = '$' + vatCharge;
+
+    const grandTotal = totalAmount + vatCharge;
+    document.getElementById('grand-total').innerText = '$' + grandTotal;
 }
 function getInputValue(busTicket){
     const busTicketInput = document.getElementById(busTicket + '-count');
     const busTicketCount = parseInt(busTicketInput.value);
     return busTicketCount
-
 }
+const bookingBtn = document.getElementById('booking');
+bookingBtn.addEventListener('click', function(){
+    const bookingArea = document.getElementById('booking-area');
+    bookingArea.style.display = 'none';
+    const successArea = document.getElementById('success-area');
+    successArea.style.display = 'block'
+})
 
 
 // function handleFastTicketChange(isIncrease){
